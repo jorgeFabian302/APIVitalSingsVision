e2220336fd51a42804a6230aa0255a8512faf0a9
Kenny <kenneth.kep@gmail.com>
2024-03-08 03:17:27 -0600

    Avance End Points
    Co-authored-by: Brian Alejandro <poenix111@users.noreply.github.com>

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 from flask import Flask, jsonify, request
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from models.User import User
-
+from models.User import User, Paciente, Familiar, Pariente
+from datetime import datetime
 # Creamos el local host para para poder utilizar la API
 
 app = Flask(__name__)
-engine = create_engine('sqlite:///NewDB.db')
+engine = create_engine('sqlite:///VitalSingSVisionDB.db')
 session = sessionmaker(bind=engine)
 session.configure(bind=engine)
 
@@ -17,7 +17,7 @@ def ping():
     return jsonify('Welcome to Vital Sings Monitor')
 
 
-# Ruta para seleccionar un paciente
+# Ruta para seleccionar todos los pacientes
 @app.route('/users', methods=['GET'])
 def users():
     s = session()
@@ -42,32 +42,38 @@ def users():
 
     return jsonify(result)
 
+#selecciona un paciente por ID
+@app.route('/users/<id>', methods=['GET'])
+def users_by_id(id):
+    s = session()
+    paciente = s.query(User).filter(User.IdUser == id).first()
+    result = {
+        'error' : None, 
+        'data' : paciente.to_dict(),
+        'status' : 'success',
+        'message' : 'Paciente recuperado con exito', 
+        'code' : 200
+    }
+    return jsonify(result)
 
-# @app.route('/pacientes/<id>', methods=['GET'])
-# def paciente(id):
-#     s = session()
-#     paciente = s.query(Paciente).filter(Paciente.IdPaciente == id).first()
-#     result = {
-#         'error' : None, 
-#         'data' : paciente.to_dict(),
-#         'status' : 'success',
-#         'message' : 'Paciente recuperado con exito', 
-#         'code' : 200
-#     }
-#     return jsonify(result)
-
+#Crear un usuario
 @app.route('/user/Insert', methods=['POST'])
 def create_user():
     s_sesion = session()
     data = request.json
+
+    date_birth = data['FechaNacimiento']
+    # year-month-day parse to datetime
+    date_birth = datetime.strptime(date_birth, '%Y-%m-%d')
+
     user = User(
-        IdUser=data['IdUser'],
-        Nombre=data['Nombre'],
-        Apellidos=data['Apellidos'],
-        Correo=data['Correo'],
-        Password=data['Password'],  
-        FechaNacimiento=data['FechaNacimiento'],
-        FotoPerfil=data['FotoPErfil']# hash 
+        IdUser = data['IdUser'],
+        Nombre = data['Nombre'],
+        Apellidos = data['Apellidos'],
+        Correo = data['Correo'],
+        Password = data['Password'],  
+        FechaNacimiento = date_birth,
+        FotoPerfil = data['FotoPerfil']
     )
     s_sesion.add(user)
     s_sesion.commit()
@@ -80,19 +86,153 @@ def create_user():
     }
     return jsonify(result)
 
-# @app.route('/pacientes/<id>', methods=['GET'])
-# def paciente(id):
-#     s = session()
-#     paciente = s.query(Paciente).filter(Paciente.IdPaciente == id).first()
-#     result = {
-#         'error' : None, 
-#         'data' : paciente.to_dict(),
-#         'status' : 'success',
-#         'message' : 'Paciente recuperado con exito', 
-#         'code' : 200
-#     }
-#     return jsonify(result)
+
+#Muestra los pacientes
+@app.route('/pacientes', methods=['GET'])
+def pacientes():
+    s = session()
+    li_pacientes = s.query(Paciente).all()
+
+    result_list = []
+    for paciente in li_pacientes:
+        result_list.append(paciente.to_dict())
+
+    result_pacientes = {
+        'pacientes': result_list,
+        'total': len(li_pacientes),
+    }
+
+    result = {
+        'error': None,
+        'data': result_pacientes,
+        'status': 'success',
+        'message': 'Pacientes recuperados con exito',
+        'code': 200
+    }
+
+    return jsonify(result)
+
+#Muestra un Paciente por ID
+@app.route('/pacientes/<id>', methods=['GET'])
+def paciente(id):
+    s = session()
+    paciente = s.query(Paciente).filter(Paciente.IdPaciente == id).first()
+    result = {
+        'error' : None, 
+        'data' : paciente.to_dict(),
+        'status' : 'success',
+        'message' : 'Paciente recuperado con exito', 
+        'code' : 200
+    }
+    return jsonify(result)
+
+#Crea un paciente
+@app.route('/paciente/Insert', methods=['POST'])
+def create_paciente():
+    s_sesion = session()
+    data = request.json
+
+    paciente = Paciente(
+        IdPaciente = data['IdPaciente'],
+        NumeroSeguroSocial = data['NumeroSeguroSocial'],
+    )
+    s_sesion.add(paciente)
+    s_sesion.commit()
+    result = {
+        'error': None,
+        'data': paciente.to_dict(),
+        'status': 'success',
+        'message': 'Paciente creado con exito',
+        'code': 201
+    }
+    return jsonify(result)
+
+#Loguin de los Usuarios
+@app.route('/login', methods=['POST'])
+def login():
+    s = session()
+    data = request.json
+    user = s.query(User).filter(User.Correo == data['Correo']).filter(User.Password == data['Password']).first()
+    if user:
+        result = {
+            'error': None,
+            'data': user.to_dict(),
+            'status': 'success',
+            'message': 'Usuario logeado con exito',
+            'code': 200
+        }
+    else:
+        result = {
+            'error': 'Usuario no encontrado',
+            'data': None,
+            'status': 'error',
+            'message': 'Usuario no encontrado',
+            'code': 404
+        }
+    return jsonify(result)
+
+#Enlista los pacientes de un Familiar
+@app.route('/getPacientesByFamiliar/<id>', methods=['GET'])
+def get_pacientes_by_familiar(id):
+    s = session()
+    parientes = s.query(Pariente).filter(Pariente.IDFFamiliar == id).all()
+    result_list = []
+    for pariente in parientes:
+        result_list.append(pariente.paciente.to_dict())
+
+    result_pacientes = {
+        'pacientes': result_list,
+        'total': len(parientes),
+    }
+
+    result = {
+        'error': None,
+        'data': result_pacientes,
+        'status': 'success',
+        'message': 'Pacientes recuperados con exito',
+        'code': 200
+    }
+
+    return jsonify(result)
+
+#Consulta a familiares
+@app.route('/familiares', methods=['GET'])
+def familiares():
+    s = session()
+    li_familiares = s.query(Familiar).all()
+
+    result_list = []
+    for familiar in li_familiares:
+        result_list.append(familiar.to_dict())
+
+    result_familiares = {
+        'familiares': result_list,
+        'total': len(li_familiares),
+    }
+
+    result = {
+        'error': None,
+        'data': result_familiares,
+        'status': 'success',
+        'message': 'Familiares recuperados con exito',
+        'code': 200
+    }
+
+    return jsonify(result)
+
+#Muestra un Paciente por ID
+@app.route('/pacientes/<id>', methods=['GET'])
+def paciente(id):
+    s = session()
+    paciente = s.query(Paciente).filter(Paciente.IdPaciente == id).first()
+    result = {
+        'error' : None, 
+        'data' : paciente.to_dict(),
+        'status' : 'success',
+        'message' : 'Paciente recuperado con exito', 
+        'code' : 200
+    }
+    return jsonify(result)
 
 if __name__ == '__main__':
-    app.run(debug=True)
-
+    app.run(debug=True, port=4000)
--- models/User.py
+++ models/User.py
@@ -1,18 +1,19 @@
-from sqlalchemy import Column, Integer, String, Date
-from sqlalchemy.orm import declarative_base
-Base = declarative_base()
+from sqlalchemy import Column, String, Date, ForeignKey, Boolean, Float, DateTime
+from sqlalchemy.orm import declarative_base, relationship
 
+
+Base = declarative_base()
 class User(Base):
     __tablename__ = 'User'
 
-    IdUser =  Column(Integer, primary_key=True)
+    IdUser =  Column(String, primary_key=True)
     Nombre = Column(String)
     Apellidos = Column(String)
     Correo = Column(String)
     Password = Column(String)
-    Tipo = Column(String)
     FechaNacimiento = Column(Date)
     FotoPerfil = Column(String)
+
     
     def to_dict(self):
         fec_nac = self.FechaNacimiento.strftime("%Y-%m-%d") if self.FechaNacimiento else None
@@ -21,7 +22,165 @@ class User(Base):
             "Nombre": self.Nombre,
             "Apellidos": self.Apellidos,
             "Correo": self.Correo,
-            "Tipo": self.Tipo,
             "FechaNacimiento": fec_nac,
-            "FotoPerfil": self.FotoPerfil
+            "FotoPerfil": self.FotoPerfil,
+        }
+    
+class Paciente(Base):
+    __tablename__ = 'Paciente'
+
+    IdPaciente =  Column(String, ForeignKey('User.IdUser'), primary_key=True)
+    NumeroSeguroSocial = Column(String, nullable=False)
+    user = relationship("User")
+
+    
+    def to_dict(self):
+
+        return {
+            "IdPaciente": self.IdPaciente,
+            "NumeroSeguroSocial": self.NumeroSeguroSocial,
+            "user": self.user.to_dict()
+        }
+
+class Familiar(Base):
+    __tablename__ = 'Familiar'
+
+    IdFamiliar =  Column(String,ForeignKey('User.IdUser') , primary_key=True)
+    NumeroTelefono = Column(String)
+    user = relationship("User")
+    
+    def to_dict(self):
+
+        return {
+            "IdFamiliar": self.IdFamiliar,
+            "NumeroTelefono": self.NumeroTelefono,
+            "user": self.user.to_dict()
+        }
+
+class Pariente(Base):
+    __tablename__ = 'Pariente'
+
+    IdPariente =  Column(String, primary_key=True)
+    IDFFamiliar = Column(String, ForeignKey('Familiar.IdFamiliar'))
+    IDFPaciente = Column(String, ForeignKey('Paciente.IdPaciente'))
+    familiar = relationship("Familiar")
+    paciente = relationship("Paciente")
+
+    
+    def to_dict(self):
+
+        return {
+            "IdPariente": self.IdPariente,
+            "IdFFamiliar": self.IDFFamiliar,
+            "IdFPaciente": self.IDFPaciente,
+            "familiar": self.familiar.to_dict(),
+            "paciente": self.paciente.to_dict()
+        }
+
+class Roles(Base):
+    __tablename__ = 'Roles'
+
+    IdUser =  Column(String, ForeignKey('User.IdUser'), primary_key=True)
+    RolPaciente = Column(Boolean)
+    RolDoctor = Column(Boolean)
+    RolFamiliar = Column(Boolean)
+
+    user = relationship("User")
+
+    def to_dict(self):
+
+        return {
+            "IdUser" : self.IdUser,
+            "RolPaciente" : self.RolPaciente,
+            "RolDoctor" : self.RolDoctor,
+            "RolFamiliar" : self.RolFamiliar,
+            "user": self.user.to_dict()
+        }
+    
+
+class Doctor(Base):
+    __tablename__ = 'Doctor'
+
+    IdDoctor =  Column(String, ForeignKey('User.IdUser'), primary_key=True)
+    Cedula = Column(String)
+    Especialidad = Column(String)
+
+    user = relationship("User")
+
+    def to_dict(self):
+
+        return {
+            "IdDoctor" : self.IdDoctor,
+            "Cedula" : self.Cedula,
+            "Especialidad" : self.Especialidad,
+            "user": self.user.to_dict()
+        }
+    
+class RevisionCardiaca(Base):
+    __tablename__ = 'RevisionCardiaca'
+
+    IdRevisionCa =  Column(String, primary_key=True)
+    imgFrecuencia = Column(String)
+    PrimerPuntoX = Column(Float)
+    PrimerPuntoY = Column(Float)
+    PuntoMasAltoX = Column(Float)
+    PuntoMasAltoY = Column(Float)
+    PuntoFinalX = Column(Float)	
+    PuntoFinalY	= Column(Float)
+    QSignalX = Column(Float) 
+    QSignalY = Column(Float)
+    SSignalX = Column(Float)
+    SSignalY = Column(Float)
+    TSignalX = Column(Float)
+    TSignalY = Column(Float)
+    PSignalX = Column(Float)
+    PSignalY = Column(Float)
+
+    def to_dict(self):
+
+        return {
+            "IdRevisionCa" : self.IdRevisionCa,
+            "imgFrecuencia" : self.imgFrecuencia, 
+            "PrimerPuntoX" : self.PrimerPuntoX,
+            "PrimerPuntoY" : self.PrimerPuntoY,
+            "PuntoMasAltoX" : self.PuntoMasAltoX,
+            "PuntoMasAltoY" : self.PuntoMasAltoY,
+            "PuntoFinalX" : self.PuntoFinalX,
+            "PuntoFinalY" : self.PuntoFinalY,
+            "QSignalX" : self.QSignalX, 
+            "QSignalY" : self.QSignalY,
+            "SSignalX" : self.SSignalX,
+            "SSignalY" : self.SSignalY,
+            "TSignalX" : self.TSignalX,
+            "TSignalY" : self.TSignalY,
+            "PSignalX" : self.PSignalX,
+            "PSignalY" : self.PSignalY,            
+        }
+
+class Consulta(Base):
+    __tablename__ = 'Consulta'
+
+    IdConsulta = Column(String, primary_key=True)
+    FechaConsulta = Column(Date)
+    HoraConsulta = Column(DateTime)
+    IdFDoctor = Column(String, ForeignKey('Doctor.IdDoctor'))
+    IdFPaciente = Column(String, ForeignKey('Paciente.IdPaciente'))
+    IdFRevisionCa = Column(String, ForeignKey('RevisionCardiaca.IdRevisionCa'))
+    Estado = Column(String)
+    FrecuenciaCardiaca = Column(Float)
+    
+    #user = relationship("User")
+
+    def to_dict(self):
+
+        return {
+            "IdConsulta" : self.IdConsulta,
+            "FechaConsulta" : self.FechaConsulta,
+            "HoraConsulta" : self.HoraConsulta,
+            "IdFDoctor" : self.IdFDoctor,
+            "IdFPaciente" : self.IdFPaciente,
+            "IdFRevisionCa" : self.IdFRevisionCa,
+            "Estado" : self.Estado,
+            "FrecuenciaCardiaca" : self.FrecuenciaCardiaca,
+            #"user": self.user.to_dict()
         }
