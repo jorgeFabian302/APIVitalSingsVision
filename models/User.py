bb281427438e03074b977e3735eb36c27dd96209
jorgeFabian302 <126297370+jorgeFabian302@users.noreply.github.com>
2024-03-07 21:19:48 -0600

    cambios de api para que funcion con urm

---
+++ models/User.py
@@ -0,0 +1,28 @@
+from sqlalchemy import Column, Integer, String, Date
+from sqlalchemy.orm import declarative_base
+import datetime
+Base = declarative_base()
+
+class User(Base):
+    __tablename__ = 'User'
+
+    IdUser =  Column(Integer, primary_key=True)
+    Nombre = Column(String)
+    Apellidos = Column(String)
+    Correo = Column(String)
+    Password = Column(String)
+    Tipo = Column(String)
+    FechaNacimiento = Column(Date)
+    FotoPerfil = Column(String)
+    
+    def to_dict(self):
+        fec_nac = self.FechaNacimiento.strftime("%Y-%m-%d") if self.FechaNacimiento else None
+        return {
+            "IdUser": self.IdUser,
+            "Nombre": self.Nombre,
+            "Apellidos": self.Apellidos,
+            "Correo": self.Correo,
+            "Tipo": self.Tipo,
+            "FechaNacimiento": fec_nac,
+            "FotoPerfil": self.FotoPerfil
+        }
